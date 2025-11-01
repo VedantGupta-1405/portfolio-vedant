55194fc9a4dfb94d2f76575f88ac18f65d7f911f
VedantGupta-1405 <vedantguptadsa@gmail.com>
2025-11-01 14:46:41 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,7 +7,7 @@ const Projects = () => {
   const projects = [
     {
       title: "F1-Simplified App",
-      description: "An app that simplifies Formula 1 by showcasing drivers, teams, and race insights in an intuitive way.",
+      description: "An app that simplifies Formula 1 by showcasing drivers and race insights in an intuitive way.",
       tech: ["React Native", "Expo", "MongoDB", "FastAPI"],
       github: "https://github.com/VedantGupta-1405/F1_Simplified",
       demo: "https://project1-demo.com",
